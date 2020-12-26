c6b1d32f10e5ebf7e828bbadf2d5bf8c3fe36d49
ramy <ramy.niranjan@gmail.com>
2020-12-27 00:58:51 +0100

    Add some style

--- src/components/People.js
+++ src/components/People.js
@@ -3,7 +3,7 @@ import React from "react";
 const People = ({ person }) => {
   return (
     <div className="flex flex-col bg-gray-800 justify-around mb-4 px-4 py-6 text-yellow-300">
-      <h3>{person.name}</h3>
+      <h3 className="text-xl font-bold">{person.name}</h3>
       <p>Gender : {person.gender}</p>
       <p>Birth Year : {person.birth_year}</p>
     </div>
--- src/components/Planet.js
+++ src/components/Planet.js
@@ -3,7 +3,7 @@ import React from "react";
 const Planet = ({ planet }) => {
   return (
     <div className="flex flex-col bg-gray-800 justify-around mb-4 px-4 py-6 text-yellow-300">
-      <h3>{planet.name}</h3>
+      <h3 className="text-xl font-bold">{planet.name}</h3>
       <p>Population : {planet.population}</p>
       <p>Terrain : {planet.terrain}</p>
     </div>
